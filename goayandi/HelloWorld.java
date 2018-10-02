e2205d71bc5fa253b30f2555698ebad5d3b00cc6
goayandi <r.borutha@gmail.com>
2018-10-02 18:34:20 +0200

    Hello World in Java

---
+++ goayandi/HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+
+   public static void main(String []args) {
+        String hw1 = "He";
+        String hw2 = "l";
+        String hw3 = "o";
+        String hw4 = "W";
+        String hw5 = "r";
+        String hw6 = "d";
+
+    System.out.println(hw1+hw2+hw2+hw3+" "+hw4+hw3+hw5+hw2+hw6);
+
+   }
+}
